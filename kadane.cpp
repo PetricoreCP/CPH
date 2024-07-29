afbfee32643315ead5a1881796f0dee7c40a48ce
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2024-07-29 18:26:42 +0200

    Maximum Subarray Sum: Kadane's algorithm

---
+++ kadane.cpp
@@ -0,0 +1,11 @@
+//Kadane's algorithm
+
+//O(n)
+long long kadane(vector<long long> &v) {
+    long long best = -1e18, curr = -1e18;
+    for(int x = 0; x < v.size(); x ++) {
+        curr = max(curr + v[x], v[x]);
+        best = max(best, curr);
+    }
+    return best;
+}
