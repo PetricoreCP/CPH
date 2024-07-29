811abc26215edeeb1cf5778be28da824c8abea57
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2024-07-29 23:29:56 +0200

    Complete Search: Generating Subsets

--- genSubs.cpp
+++ genSubs.cpp
@@ -1,6 +1,7 @@
 //Generating Subsets
 
 //O(2^(n))
+//Iterative
 vector<vector<long long>> genSubs(vector<long long> &v) {
     vector<vector<long long>> subsets;
     for(int i = 0; i < (1 << v.size()); i ++) {
@@ -13,4 +14,20 @@ vector<vector<long long>> genSubs(vector<long long> &v) {
         subsets.push_back(s);
     }
     return subsets;
+}
+
+//Recursive
+vector<vector<long long>> subsets;
+vector<long long> s;
+
+void genSubs(vector<long long> &v, int i) {
+    if(i == v.size()) {
+        subsets.push_back(s);
+    }
+    else {
+        genSubs(v, i + 1);
+        s.push_back(v[i]);
+        genSubs(v, i + 1);
+        s.pop_back();
+    }
 }
