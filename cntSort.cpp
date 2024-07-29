fb69979b0f09ecb39795ac0dddb1937da0003312
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2024-07-29 22:22:06 +0200

    Sorting: Counting Sort

--- cntSort.cpp
+++ cntSort.cpp
@@ -1,7 +1,7 @@
 //Counting Sort
 
 //O(n)
-void countingSort(vector<int> &v, int m) {
+void cntSort(vector<int> &v, int m) {
     vector<int> freq(m + 1);
     for(int i = 0; i < v.size(); i ++) {
         freq[v[i]] ++;
